3d71cbdeaadf1f22893c2d38488311c8e12c7231
Ryodai-15 <gon.4629.1208@gmail.com>
2021-09-23 07:20:14 +0900

    [finish]chapter4

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,14 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String name;
+		final int lucky = 15;
+		name = "Ryodai";
+		System.out.println(name);
+
+		name = "Gon";
+		System.out.println(name);
+
+		System.out.println(lucky);
+	}
+}
